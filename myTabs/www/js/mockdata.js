8e8ef8f6ca45cf168cd326ef6b9d11cdad7ccf10
Your Name <you@example.com>
2016-08-31 16:12:52 +0800

    myTabs

---
+++ myTabs/www/js/mockdata.js
@@ -0,0 +1,12 @@
+Mock.mock('/homepage/init', {
+    'name'	   : '[@name](/user/name)()',
+    'age|1-100': 100,
+    'color'	   : '[@color](/user/color)',
+	"prizes|1-100": [// 获奖信息
+                {
+                    "name": "@ctitle", // 获奖名称
+                    "prizeDate": "@date", // 获奖时间
+                    "prizeLevel": "一等奖" // 获奖等级
+                }
+            ]
+});
